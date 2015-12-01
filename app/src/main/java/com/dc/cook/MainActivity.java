cf8dd55f6564cc867cbd5903c99a5decf633e860
James Kozianski <soulplant@gmail.com>
2015-12-02 07:25:55 +1100

    Remove unused import.

--- app/src/main/java/com/dc/cook/MainActivity.java
+++ app/src/main/java/com/dc/cook/MainActivity.java
@@ -3,7 +3,6 @@ package com.dc.cook;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
